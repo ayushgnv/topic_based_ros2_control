cff77b872ece718826c6ca2a5f85ab1ad9c3c678
Ayush Ghosh <ayushg@nvidia.com>
2026-07-02 14:01:44 -0700

    Format code

--- test/test_topic_based_robot.py
+++ test/test_topic_based_robot.py
@@ -39,9 +39,7 @@ robot = TopicBasedRobot(["joint_1", "joint_2", "joint_3"])
 # By default the joint_states should have the values from initial_value from rrr.urdf.xacro
 current_joint_state = robot.get_current_joint_state()
 urdf_initial_values = [0.2, 0.3, 0.1]
-assert (
-    current_joint_state == urdf_initial_values
-), f"{current_joint_state=} != {urdf_initial_values=}"
+assert current_joint_state == urdf_initial_values, f"{current_joint_state=} != {urdf_initial_values=}"
 
 # Test setting the robot joint states
 joint_state = [0.1, 0.2, 0.3]
